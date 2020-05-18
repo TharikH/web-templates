6e15b3e4b7e6c06973696c6fef5af4563e600060
Tharikh <tharikhlegend@gmail.com>
2020-05-18 19:09:21 +0530

    started other templates

--- wesite1/index.js
+++ wesite1/index.js
@@ -4,4 +4,22 @@ $(document).ready(function () {
         $(".nav-button-table").slideToggle("slow");
         menu.toggleClass("animate");
         });
-    });
+    animate_image(".img-div h1","INTERSTELLAR SPACE!",0,100);
+});
+
+function animate_image(target, message, index, interval) 
+  {    
+    if (index < message.length) 
+    { 
+      $(target).append(message[index++]); 
+      setTimeout(function () { animate_image(target, message, index, interval); }, interval); 
+    }else{
+        $(target).css("color", "black");
+        setTimeout(()=>{
+        index=0;
+        $(target).css("color", "rgb(107, 188, 255)");
+        $(target).text('');
+        animate_image(target, message, index, interval);
+        },2000);
+    }
+  }
